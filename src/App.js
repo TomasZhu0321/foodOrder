013ae3b723925cb901fcb1bffaa6f1ec79f8c90e
ThomasZhu <tomaszhu0321@gmail.com>
2022-08-21 16:40:18 -0400

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Fragment } from "react";
+import Header from "./Layout/Header";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header>
+      </Header>
+    </Fragment>
   );
 }
 
