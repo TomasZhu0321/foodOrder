c9acf4d7288e535f87d97a700bd5123d65814306
ThomasZhu <tomaszhu0321@gmail.com>
2022-08-22 11:35:29 -0400

    add form

--- src/Meals/AvailableMeals.js
+++ src/Meals/AvailableMeals.js
@@ -1,39 +1,49 @@
-import classes from './AvailableMeals.module.css';
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
-    const DUMMY_MEALS = [
-        {
-          id: 'm1',
-          name: 'Sushi',
-          description: 'Finest fish and veggies',
-          price: 22.99,
-        },
-        {
-          id: 'm2',
-          name: 'Schnitzel',
-          description: 'A german specialty!',
-          price: 16.5,
-        },
-        {
-          id: 'm3',
-          name: 'Barbecue Burger',
-          description: 'American, raw, meaty',
-          price: 12.99,
-        },
-        {
-          id: 'm4',
-          name: 'Green Bowl',
-          description: 'Healthy...and green...',
-          price: 18.99,
-        },
-      ];
-
-const AvailableMeals = () =>{
-    const mealslist = DUMMY_MEALS.map((meals)=> <li>{meals.name}</li>)
-    return(
-        <section className={classes.meals}>
-            <ul>{mealslist}</ul>
-        </section>
-    )
-}
+const AvailableMeals = () => {
+  const mealslist = DUMMY_MEALS.map((meals) => (
+    <MealItem
+      key={meals.id}
+      name={meals.name}
+      description={meals.description}
+      price={meals.price}
+    />
+  ));
+  return (
+    <Card>
+      <section className={classes.meals}>
+        <ul>{mealslist}</ul>
+      </section>
+    </Card>
+  );
+};
 
 export default AvailableMeals;
